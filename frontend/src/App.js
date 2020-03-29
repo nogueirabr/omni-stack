01014a89a106e65b45eae46f034ddaefb49d5863
michael <michael.nogueira@msn.com>
2020-03-29 23:20:29 +0100

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //necessario da div para por um elemento abaixo do outro no react
+    <div>
+      <Routes />
     </div>
   );
 }
